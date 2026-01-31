6ae422e5c1bdbdbd01e3a293a1a2aa33eb7283b1
Nicolas Rodi <nicolasgeronimorodi@gmail.com>
2026-01-31 17:59:17 -0300

    fix: fixed locale for date

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { routes } from './app.routes';
 import { WebSpeechRecognitionService } from './services/speech-to-text/web-speech-recognition.service';
@@ -12,6 +14,34 @@ import Aura from '@primeng/themes/aura';
 import { DarkPreset, MyPreset } from '../styles';
 import { MessageService } from 'primeng/api';
 
+registerLocaleData(localeEs, 'es');
+
+const spanishTranslation = {
+  dayNames: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+  dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+  dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+  monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+  today: 'Hoy',
+  clear: 'Limpiar',
+  weekHeader: 'Sem',
+  firstDayOfWeek: 1,
+  dateFormat: 'dd/mm/yy'
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), WebSpeechRecognitionService, WhisperRecognitionService, provideAnimations(), providePrimeNG({theme: {preset: DarkPreset, options: {darkModeSelector: '.app-dark'}}}), MessageService  ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    WebSpeechRecognitionService,
+    WhisperRecognitionService,
+    provideAnimations(),
+    providePrimeNG({
+      theme: { preset: DarkPreset, options: { darkModeSelector: '.app-dark' } },
+      translation: spanishTranslation
+    }),
+    MessageService,
+    { provide: LOCALE_ID, useValue: 'es' }
+  ]
 };
